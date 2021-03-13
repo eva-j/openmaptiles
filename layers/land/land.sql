4200ff518a8e2304d9f9ca8e856a5f2b9f806bd9
Eva J <evka.jelinkova@gmail.com>
2021-03-13 22:09:38 +0100

    preparing indexed tables to not overload generate-tiles

--- layers/land/land.sql
+++ layers/land/land.sql
@@ -76,9 +76,10 @@ CREATE INDEX IF NOT EXISTS ne_110m_land_gen_z0_idx ON ne_110m_land_gen_z0 USING 
 
 
 
--- etldoc:  ne_110m_land_gen_z0 ->  land_z0
--- etldoc:  ne_110m_lakes_gen_z0 ->  land_z0
-CREATE OR REPLACE VIEW land_z0 AS
+-- etldoc:  ne_110m_land_gen_z0 ->  land_polygon_z0
+-- etldoc:  ne_110m_lakes_gen_z0 ->  land_polygon_z0
+DROP TABLE IF EXISTS land_polygon_z0 CASCADE;
+CREATE TABLE land_polygon_z0 AS
 (
 WITH temp AS
     (
@@ -90,10 +91,12 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        l.class
 FROM ne_110m_land_gen_z0 l LEFT JOIN temp t ON l.ogc_fid = t.ogc_fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z0_idx ON land_polygon_z0 USING gist (geometry);
 
--- etldoc:  ne_110m_land_gen_z1 ->  land_z1
--- etldoc:  ne_110m_lakes_gen_z1 ->  land_z1
-CREATE OR REPLACE VIEW land_z1 AS
+-- etldoc:  ne_110m_land_gen_z1 ->  land_polygon_z1
+-- etldoc:  ne_110m_lakes_gen_z1 ->  land_polygon_z1
+DROP TABLE IF EXISTS land_polygon_z1 CASCADE;
+CREATE TABLE land_polygon_z1 AS
 (
 WITH temp AS
     (
@@ -105,10 +108,12 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        l.class
 FROM ne_110m_land_gen_z1 l LEFT JOIN temp t ON l.ogc_fid = t.ogc_fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z1_idx ON land_polygon_z1 USING gist (geometry);
 
--- etldoc:  ne_50m_land_gen_z2 ->  land_z2
--- etldoc:  ne_50m_lakes_gen_z2 ->  land_z2
-CREATE OR REPLACE VIEW land_z2 AS
+-- etldoc:  ne_50m_land_gen_z2 ->  land_polygon_z2
+-- etldoc:  ne_50m_lakes_gen_z2 ->  land_polygon_z2
+DROP TABLE IF EXISTS land_polygon_z2 CASCADE;
+CREATE TABLE land_polygon_z2 AS
 (
 WITH temp AS
     (
@@ -120,10 +125,12 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        l.class
 FROM ne_50m_land_gen_z2 l LEFT JOIN temp t ON l.ogc_fid = t.ogc_fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z2_idx ON land_polygon_z2 USING gist (geometry);
 
--- etldoc:  ne_50m_land_gen_z3 ->  land_z3
--- etldoc:  ne_50m_lakes_gen_z3 ->  land_z3
-CREATE OR REPLACE VIEW land_z3 AS
+-- etldoc:  ne_50m_land_gen_z3 ->  land_polygon_z3
+-- etldoc:  ne_50m_lakes_gen_z3 ->  land_polygon_z3
+DROP TABLE IF EXISTS land_polygon_z3 CASCADE;
+CREATE TABLE land_polygon_z3 AS
 (
 WITH temp AS
     (
@@ -135,10 +142,12 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        l.class
 FROM ne_50m_land_gen_z3 l LEFT JOIN temp t ON l.ogc_fid = t.ogc_fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z3_idx ON land_polygon_z3 USING gist (geometry);
 
--- etldoc:  ne_50m_land_gen_z4 ->  land_z4
--- etldoc:  ne_10m_lakes_gen_z4 ->  land_z4
-CREATE OR REPLACE VIEW land_z4 AS
+-- etldoc:  ne_50m_land_gen_z4 ->  land_polygon_z4
+-- etldoc:  ne_10m_lakes_gen_z4 ->  land_polygon_z4
+DROP TABLE IF EXISTS land_polygon_z4 CASCADE;
+CREATE TABLE land_polygon_z4 AS
 (
 WITH temp AS
     (
@@ -151,10 +160,12 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        l.class
 FROM ne_50m_land_gen_z4 l LEFT JOIN temp t ON l.ogc_fid = t.ogc_fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z4_idx ON land_polygon_z4 USING gist (geometry);
 
--- etldoc:  ne_10m_land_gen_z5 ->  land_z5
--- etldoc:  ne_10m_lakes_gen_z5 ->  land_z5
-CREATE OR REPLACE VIEW land_z5 AS
+-- etldoc:  ne_10m_land_gen_z5 ->  land_polygon_z5
+-- etldoc:  ne_10m_lakes_gen_z5 ->  land_polygon_z5
+DROP TABLE IF EXISTS land_polygon_z5 CASCADE;
+CREATE TABLE land_polygon_z5 AS
 (
 WITH temp AS
     (
@@ -167,10 +178,12 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        l.class
 FROM ne_10m_land_gen_z5 l LEFT JOIN temp t ON l.fid = t.fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z5_idx ON land_polygon_z5 USING gist (geometry);
 
--- etldoc:  osm_land_polygon_gen_z6 ->  land_z6
--- etldoc:  osm_water_polygon_gen_z6 ->  land_z6
-CREATE OR REPLACE VIEW land_z6 AS
+-- etldoc:  osm_land_polygon_gen_z6 ->  land_polygon_z6
+-- etldoc:  osm_water_polygon_gen_z6 ->  land_polygon_z6
+DROP TABLE IF EXISTS land_polygon_z6 CASCADE;
+CREATE TABLE land_polygon_z6 AS
 (
 WITH temp AS
     (
@@ -182,10 +195,12 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        'land'::text AS class
 FROM osm_land_polygon_gen_z6 l LEFT JOIN temp t ON l.fid = t.fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z6_idx ON land_polygon_z6 USING gist (geometry);
 
--- etldoc:  osm_land_polygon_gen_z7 ->  land_z7
--- etldoc:  osm_water_polygon_gen_z7 ->  land_z7
-CREATE OR REPLACE VIEW land_z7 AS
+-- etldoc:  osm_land_polygon_gen_z7 ->  land_polygon_z7
+-- etldoc:  osm_water_polygon_gen_z7 ->  land_polygon_z7
+DROP TABLE IF EXISTS land_polygon_z7 CASCADE;
+CREATE TABLE land_polygon_z7 AS
 (
 WITH temp AS
     (
@@ -197,10 +212,12 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        'land'::text AS class
 FROM osm_land_polygon_gen_z7 l LEFT JOIN temp t ON l.fid = t.fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z7_idx ON land_polygon_z7 USING gist (geometry);
 
--- etldoc:  osm_land_polygon_gen_z8 ->  land_z8
--- etldoc:  osm_water_polygon_gen_z8 ->  land_z8
-CREATE OR REPLACE VIEW land_z8 AS
+-- etldoc:  osm_land_polygon_gen_z8 ->  land_polygon_z8
+-- etldoc:  osm_water_polygon_gen_z8 ->  land_polygon_z8
+DROP TABLE IF EXISTS land_polygon_z8 CASCADE;
+CREATE TABLE land_polygon_z8 AS
 (
 WITH temp AS
     (
@@ -212,10 +229,12 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        'land'::text AS class
 FROM osm_land_polygon_gen_z8 l LEFT JOIN temp t ON l.fid = t.fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z8_idx ON land_polygon_z8 USING gist (geometry);
 
--- etldoc:  osm_land_polygon_gen_z9 ->  land_z9
--- etldoc:  osm_water_polygon_gen_z9 ->  land_z9
-CREATE OR REPLACE VIEW land_z9 AS
+-- etldoc:  osm_land_polygon_gen_z9 ->  land_polygon_z9
+-- etldoc:  osm_water_polygon_gen_z9 ->  land_polygon_z9
+DROP TABLE IF EXISTS land_polygon_z9 CASCADE;
+CREATE TABLE land_polygon_z9 AS
 (
 WITH temp AS
     (
@@ -227,10 +246,12 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        'land'::text AS class
 FROM osm_land_polygon_gen_z9 l LEFT JOIN temp t ON l.fid = t.fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z9_idx ON land_polygon_z9 USING gist (geometry);
 
--- etldoc:  osm_land_polygon_gen_z10 ->  land_z10
--- etldoc:  osm_water_polygon_gen_z10 ->  land_z10
-CREATE OR REPLACE VIEW land_z10 AS
+-- etldoc:  osm_land_polygon_gen_z10 ->  land_polygon_z10
+-- etldoc:  osm_water_polygon_gen_z10 ->  land_polygon_z10
+DROP TABLE IF EXISTS land_polygon_z10 CASCADE;
+CREATE TABLE land_polygon_z10 AS
 (
 WITH temp AS
     (
@@ -242,10 +263,12 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        'land'::text AS class
 FROM osm_land_polygon_gen_z10 l LEFT JOIN temp t ON l.fid = t.fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z10_idx ON land_polygon_z10 USING gist (geometry);
 
--- etldoc:  osm_land_polygon_gen_z11 ->  land_z11
--- etldoc:  osm_water_polygon_gen_z11 ->  land_z11
-CREATE OR REPLACE VIEW land_z11 AS
+-- etldoc:  osm_land_polygon_gen_z11 ->  land_polygon_z11
+-- etldoc:  osm_water_polygon_gen_z11 ->  land_polygon_z11
+DROP TABLE IF EXISTS land_polygon_z11 CASCADE;
+CREATE TABLE land_polygon_z11 AS
 (
 WITH temp AS
     (
@@ -257,10 +280,12 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        'land'::text AS class
 FROM osm_land_polygon_gen_z11 l LEFT JOIN temp t ON l.fid = t.fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z11_idx ON land_polygon_z11 USING gist (geometry);
 
--- etldoc:  osm_land_polygon ->  land_z12
--- etldoc:  osm_water_polygon ->  land_z12
-CREATE OR REPLACE VIEW land_z12 AS
+-- etldoc:  osm_land_polygon ->  land_polygon_z12
+-- etldoc:  osm_water_polygon ->  land_polygon_z12
+DROP TABLE IF EXISTS land_polygon_z12 CASCADE;
+CREATE TABLE land_polygon_z12 AS
 (
 WITH temp AS
     (
@@ -272,6 +297,7 @@ SELECT ST_Difference(l.geometry, COALESCE(t.geometry, ST_SetSRID('GEOMETRYCOLLEC
        'land'::text AS class
 FROM osm_land_polygon l LEFT JOIN temp t ON l.fid = t.fid
     );
+CREATE INDEX IF NOT EXISTS land_polygon_z12_idx ON land_polygon_z12 USING gist (geometry);
 
 
 -- etldoc: layer_land [shape=record fillcolor=lightpink, style="rounded,filled",
@@ -288,69 +314,69 @@ $$
 SELECT geometry,
        class::text
 FROM (
-         -- etldoc: land_z0 ->  layer_land:z0
+         -- etldoc: land_polygon_z0 ->  layer_land:z0
          SELECT *
-         FROM land_z0
+         FROM land_polygon_z0
          WHERE zoom_level = 0
          UNION ALL
-         -- etldoc: land_z1 ->  layer_land:z1
+         -- etldoc: land_polygon_z1 ->  layer_land:z1
          SELECT *
-         FROM land_z1
+         FROM land_polygon_z1
          WHERE zoom_level = 1
          UNION ALL
-         -- etldoc: land_z2 ->  layer_land:z2
+         -- etldoc: land_polygon_z2 ->  layer_land:z2
          SELECT *
-         FROM land_z2
+         FROM land_polygon_z2
          WHERE zoom_level = 2
          UNION ALL
-         -- etldoc: land_z3 ->  layer_land:z3
+         -- etldoc: land_polygon_z3 ->  layer_land:z3
          SELECT *
-         FROM land_z3
+         FROM land_polygon_z3
          WHERE zoom_level = 3
          UNION ALL
-         -- etldoc: land_z4 ->  layer_land:z4
+         -- etldoc: land_polygon_z4 ->  layer_land:z4
          SELECT *
-         FROM land_z4
+         FROM land_polygon_z4
          WHERE zoom_level = 4
          UNION ALL
-         -- etldoc: land_z5 ->  layer_land:z5
+         -- etldoc: land_polygon_z5 ->  layer_land:z5
          SELECT *
-         FROM land_z5
+         FROM land_polygon_z5
          WHERE zoom_level = 5
          UNION ALL
-         -- etldoc: land_z6 ->  layer_land:z6
+         -- etldoc: land_polygon_z6 ->  layer_land:z6
          SELECT *
-         FROM land_z6
+         FROM land_polygon_z6
          WHERE zoom_level = 6
          UNION ALL
-         -- etldoc: land_z7 ->  layer_land:z7
+         -- etldoc: land_polygon_z7 ->  layer_land:z7
          SELECT *
-         FROM land_z7
+         FROM land_polygon_z7
          WHERE zoom_level = 7
          UNION ALL
-         -- etldoc: land_z8 ->  layer_land:z8
+         -- etldoc: land_polygon_z8 ->  layer_land:z8
          SELECT *
-         FROM land_z8
+         FROM land_polygon_z8
          WHERE zoom_level = 8
          UNION ALL
-         -- etldoc: land_z9 ->  layer_land:z9
+         -- etldoc: land_polygon_z9 ->  layer_land:z9
          SELECT *
-         FROM land_z9
+         FROM land_polygon_z9
          WHERE zoom_level = 9
          UNION ALL
-         -- etldoc: land_z10 ->  layer_land:z10
+         -- etldoc: land_polygon_z10 ->  layer_land:z10
          SELECT *
-         FROM land_z10
+         FROM land_polygon_z10
          WHERE zoom_level = 10
          UNION ALL
-         -- etldoc: land_z11 ->  layer_land:z11
+         -- etldoc: land_polygon_z11 ->  layer_land:z11
          SELECT *
-         FROM land_z11
+         FROM land_polygon_z11
          WHERE zoom_level = 11
          UNION ALL
-         -- etldoc: land_z12 ->  layer_land:z12
+         -- etldoc: land_polygon_z12 ->  layer_land:z12
          SELECT *
-         FROM land_z12
+         FROM land_polygon_z12
          WHERE zoom_level >= 12
      ) AS zoom_levels
 WHERE geometry && bbox;
